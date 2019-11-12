f81644d28239d065487188f392944abd21e17a49
zotovacaha <46525867+zotovacaha@users.noreply.github.com>
2019-11-13 00:22:49 +0300

    Create StudentNerd.cpp

---
+++ StudentNerd.cpp
@@ -0,0 +1,17 @@
+#include "StudentNerd.h"
+
+StudentNerd::StudentNerd(string name): Student(name, (4 + rand() % 2))
+{
+}
+
+int StudentNerd::doTheExam()
+{
+	return 4 + rand()%2;
+}
+int StudentNerd::doTheRetakeExam()
+{
+	return 4 + rand() % 2;
+}
+StudentNerd::~StudentNerd()
+{
+}
