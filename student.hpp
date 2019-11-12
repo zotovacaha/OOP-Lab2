0830316e9220d9b69802188ff86fb303274a8404
zotovacaha <46525867+zotovacaha@users.noreply.github.com>
2019-11-13 00:24:16 +0300

    Create student.hpp

---
+++ student.hpp
@@ -0,0 +1,36 @@
+
+#ifndef student_hpp
+#define student_hpp
+
+#include <stdio.h>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+class StudentDefault;
+class StudentNerd;
+class StudentCouncilMember;
+
+class Student
+{
+private:
+    string name;
+    int min_grade;
+    vector<int> groupNumbers;
+public:
+    void addGroupNumber(int groupNumber);
+    void deleteGroupNumber(int groupNumber);
+    string getName();
+    void setName(string name);
+	virtual int doTheExam() = 0;
+	virtual int doTheRetakeExam() = 0;
+	StudentDefault* switchToDefaultStudent();
+	StudentNerd* switchToNerd();
+	StudentCouncilMember* switchToCouncilMember();
+    int getGroupCount();
+    int getMinGrade();
+    const vector<int>& getGroupNumbers();
+    Student(string name, int min_grade);
+    Student();
+};
+#endif 
