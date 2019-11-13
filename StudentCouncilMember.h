efcea170c279c593bd8dd3f4fd267446dd5d3fe8
zotovacaha <46525867+zotovacaha@users.noreply.github.com>
2019-11-14 00:26:00 +0300

    Update StudentCouncilMember.h

--- StudentCouncilMember.h
+++ StudentCouncilMember.h
@@ -1,10 +1,12 @@
 #pragma once
+
 #include "StudentDefault.h"
+
 class StudentCouncilMember: public StudentDefault
 {
 public:
-	StudentCouncilMember(string name);
-
+	StudentCouncilMember(const string &name);
+	StudentCouncilMember(Student* student);
 	int doTheRetakeExam() override;
 	~StudentCouncilMember();
 };
