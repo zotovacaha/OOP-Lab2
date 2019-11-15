10126ad6b41f1d1167dcdae3777f61f25d10cb70
zotovacaha <46525867+zotovacaha@users.noreply.github.com>
2019-11-16 01:14:40 +0300

    Update Student.h

--- Student.h
+++ Student.h
@@ -1,5 +1,5 @@
-#ifndef Student_h
-#define Student_h
+#ifndef student_h
+#define student_h
 
 #include <stdio.h>
 #include <vector>
@@ -14,17 +14,18 @@ private:
     int minGrade;
     vector<int> groupNumbers;
 public:
-    void addGroupNumber(int groupNumber);
+void addGroupNumber(int groupNumber);
     void deleteGroupNumber(int groupNumber);
-    string getName();
+    const string& getName();
     void setName(const string &name);
-	virtual int doTheExam() = 0;
-	virtual int doTheRetakeExam() = 0;
+    virtual int doTheExam() = 0;
+    virtual int doTheRetakeExam() = 0;
     int getGroupCount();
     int getMinGrade();
     const vector<int>& getGroupNumbers();
     Student(const string &name, int min_grade);
     Student();
+    virtual ~Student() = default;
 };
 #endif 
 
