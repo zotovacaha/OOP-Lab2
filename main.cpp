8bfc5961759836f86a4fcb30059771486152a4ba
zotovacaha <46525867+zotovacaha@users.noreply.github.com>
2019-11-16 00:55:04 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -16,10 +15,10 @@
 using namespace std;
 
 int main(int argc, const char * argv[]) {
-	setlocale(0, "");
+    setlocale(0, "");
     srand(time(NULL));
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
     Student* listOfAllStudents[10];
     StudentGroup* listOfAllGroups[5];
     string names[10] = {"Иванов Иван Иванович",
@@ -37,19 +36,19 @@ int main(int argc, const char * argv[]) {
     int groups[5] = {111,222,333,444,555};
     for(int i=0; i<6; i++)
     {
-		StudentDefault* student = new StudentDefault(names[i]);
+	StudentDefault* student = new StudentDefault(names[i]);
         listOfAllStudents[i] = student;
     }
-	for (int i = 6; i < 8; i++)
-	{
-		StudentNerd* student = new StudentNerd(names[i]);
-		listOfAllStudents[i] = student;
-	}
-	for (int i = 8; i < 10; i++)
-	{
-		StudentCouncilMember* student = new StudentCouncilMember(names[i]);
-		listOfAllStudents[i] = student;
-	}
+    for (int i = 6; i < 8; i++)
+    {
+	StudentNerd* student = new StudentNerd(names[i]);
+	listOfAllStudents[i] = student;
+    }
+    for (int i = 8; i < 10; i++)
+    {
+	StudentCouncilMember* student = new StudentCouncilMember(names[i]);
+	listOfAllStudents[i] = student;
+    }
 
     for(int i=0; i<5; i++)
     {
@@ -80,58 +79,59 @@ int main(int argc, const char * argv[]) {
         {
             os<<"В группе "<<listOfAllGroups[i]->getGroupNumber()<<" нет студентов."<<endl;
         }
-        else{
-        os << "Студенты группы "<<listOfAllGroups[i]->getGroupNumber()<<":"<<endl;
-        for(int i=0; i<tempVector.size(); i++)
-        {
-            os<<i+1<<"."<<tempVector[i]->getName()<<endl<<"Минимальная оценка: "<<tempVector[i]->getMinGrade()<<endl<<"Кол-во групп: ";
-            os<<tempVector[i]->getGroupCount()<<endl<<"Группы: ";
-            for(int j=0; j<tempVector[i]->getGroupCount(); j++)
-            {
-                os<<tempVector[i]->getGroupNumbers()[j]<<" ";
-            }
-            os<<endl;
-        }
-        }
+        else
+	{
+             os << "Студенты группы "<<listOfAllGroups[i]->getGroupNumber()<<":"<<endl;
+             for(int i=0; i<tempVector.size(); i++)
+             {
+             	 os<<i+1<<"."<<tempVector[i]->getName()<<endl<<"Минимальная оценка: "<<tempVector[i]->getMinGrade()<<endl<<"Кол-во групп: ";
+           	 os<<tempVector[i]->getGroupCount()<<endl<<"Группы: ";
+          	 for(int j=0; j<tempVector[i]->getGroupCount(); j++)
+            	 {
+		     os<<tempVector[i]->getGroupNumbers()[j]<<" ";
+                 }
+                 os<<endl;
+              }
+	}
     }
     fb.close();
     cout<<"Список студентов всех групп был выведен в файл."<<endl;
     cout<<"Список групп: ";
     for(int i=0; i<5; i++)
     {
-      cout<<listOfAllGroups[i]->getGroupNumber()<<" ";
+      	cout<<listOfAllGroups[i]->getGroupNumber()<<" ";
     }
-	cout << endl;
-	for (int i = 0; i < 5; i++)
-	{
-		cout<< "Проведение экзамена в группе " << listOfAllGroups[i]->getGroupNumber()<< endl;
-		listOfAllGroups[i]->startExam();
-	}
-	Student* listOfAllNerdStudents[10];
-	for (int i = 0; i < 10; i++)
-	{
-		StudentNerd* tempNerd = new StudentNerd(listOfAllStudents[i]);
-		delete listOfAllStudents[i];
-		listOfAllNerdStudents[i]= tempNerd;
-	}
-	for (int i = 0; i < 5; i++)
-	{
-		listOfAllGroups[i]->deleteAllStudents();
-	}
-	for (int i = 0; i < 10; i++)
-	{
-		int indGroup = rand() % 5;
-		int indStud = rand() % 10;
-		listOfAllGroups[indGroup]->addStudent(listOfAllNerdStudents[indStud]);
-	}
-	cout << "Все студенты были переведены в ботаников. " << endl;
-	for (int i = 0; i < 5; i++)
-	{
-		cout << "Проведение экзамена в группе " << listOfAllGroups[i]->getGroupNumber() << endl;
-		listOfAllGroups[i]->startExam();
-	}
-	system("pause");
-return 0;
+    cout << endl;
+    for (int i = 0; i < 5; i++)
+    {
+	cout<< "Проведение экзамена в группе " << listOfAllGroups[i]->getGroupNumber()<< endl;
+	listOfAllGroups[i]->startExam();
+    }
+    Student* listOfAllNerdStudents[10];
+    for (int i = 0; i < 10; i++)
+    {
+	StudentNerd* tempNerd = new StudentNerd(listOfAllStudents[i]);
+	delete listOfAllStudents[i];
+	listOfAllNerdStudents[i]= tempNerd;
+    }
+    for (int i = 0; i < 5; i++)
+    {
+	listOfAllGroups[i]->deleteAllStudents();
+    }
+    for (int i = 0; i < 10; i++)
+    {
+	int indGroup = rand() % 5;
+	int indStud = rand() % 10;
+	listOfAllGroups[indGroup]->addStudent(listOfAllNerdStudents[indStud]);
+    }
+    cout << "Все студенты были переведены в ботаников. " << endl;
+    for (int i = 0; i < 5; i++)
+    {
+	cout << "Проведение экзамена в группе " << listOfAllGroups[i]->getGroupNumber() << endl;
+	listOfAllGroups[i]->startExam();
+    }
+    system("pause");
+    return 0;
 }
 
 
