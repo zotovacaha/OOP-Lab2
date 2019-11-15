51670abb4e4c6a236b5e1ace20f6e510b31e2984
zotovacaha <46525867+zotovacaha@users.noreply.github.com>
2019-11-16 00:59:52 +0300

    Update StudentDefault.cpp

--- StudentDefault.cpp
+++ StudentDefault.cpp
@@ -1,16 +1,18 @@
 #include "StudentDefault.h"
 
-StudentDefault::StudentDefault(string name): Student(name,(2 + rand() % 4))
+StudentDefault::StudentDefault(const string &name): Student(name,(2 + rand() % 4))
 {
 	
 }
+StudentDefault::StudentDefault(Student* student) : Student(student->getName(), (2 + rand() % 4))
+{
+
+}
 int StudentDefault::doTheExam()
 {
-	return 2 + rand() % 4;
+    return 2 + rand() % 4;
 }
-
 int StudentDefault::doTheRetakeExam()
 {
-	return 2 + rand() % 4;
+    return 2 + rand() % 4;
 }
-
